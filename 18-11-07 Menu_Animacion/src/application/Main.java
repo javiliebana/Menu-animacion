2843ffe12def2e73575c6bb7de37d83950a2ecc9
javier <javierliebana99@gmail.com>
2018-11-21 20:47:09 +0100

    se puede mejorar...no

---
+++ 18-11-07 Menu_Animacion/src/application/Main.java
@@ -0,0 +1,32 @@
+package application;
+	
+import javafx.application.Application;
+
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+//asd
+
+public class Main extends Application {
+
+	 @Override
+	    public void start(Stage primaryStage) throws Exception{
+
+	        FXMLLoader loader =new FXMLLoader(getClass().getResource("interfaz.fxml"));
+	        AnchorPane pane=loader.load();
+	        Scene scene=new Scene(pane);
+	        scene.getStylesheets().addAll(getClass().getResource("application.css").toExternalForm());
+	        primaryStage.setTitle("Home page");
+	        primaryStage.setScene(scene);
+	        primaryStage.show();
+
+	    }
+
+
+	    
+	    public static void main(String[] args) {
+	        launch(args);
+	    }
+	}
